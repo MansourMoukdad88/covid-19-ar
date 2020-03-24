d7919f4d4b16abe5df3d3e7d5fe06ed9642fbe96
MansourMoukdad88 <m_sy@outlook.com>
2020-03-24 10:18:38 +0200

    Add filtered news as a ticker

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import EG from "./EG";
 import LB from "./LB";
 import Sy from "./Sy";
 import Search from "./components/Search";
-
+import InstructionTicker from "./components/InstructionTicker";
 class App extends Component {
   
 
@@ -23,7 +23,8 @@ class App extends Component {
     return (
       <div className="app">
         <div className="container">
-          <h1>Covid-19  <img src={logo} className="App-logo" alt="logo" />  Corona</h1>
+          <InstructionTicker/>
+          <h1 className="app-heading">Covid-19  <img src={logo} className="App-logo" alt="logo" />  Corona</h1>
           <div style={{"textAlign":"center","margin":"10px", "backgroundColor":"#5B86E5", "width": "100px", "borderRadius":"3px"}}>{today}</div>
           <h5 style={{"fontSize": "8px", "textAlign":"left", "marginLeft":"10px"}}>Made by <a href="http://codings.io">codings.io</a></h5>
           <Search/>
---
+++ src/components/InstructionTicker.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import "../App.css"
+export default class InstructionTicker extends Component {
+
+  state={
+    title:null,
+    url:null
+  }
+
+  componentDidMount = () => {
+    fetch("http://newsapi.org/v2/top-headlines?country=ae&category=health&apiKey=f869bbb8172a4c53b25182b5fed0ca61")
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.articles);
+      let articleTitle = [];
+      let articleURL =[]
+      let articles = data.articles.forEach((article) => {
+        if(article.title.includes("كورونا")) {
+          let title = "| " + article.title + " |"
+          let url = article.url + " ";
+
+          // console.log(`<a href=${article.url}>article.title</a>`);
+          articleTitle.push(title);
+          articleURL.push(url);
+        } 
+        this.setState({title:articleTitle, url:articleURL})
+        console.log("State",this.state);
+      })      
+    })
+  }
+
+  render() {
+    let {title, url} = this.state;
+console.log("render title", title);
+console.log("render url", url);
+
+    return (
+      <div className="tcontainer">
+        <div className="ticker-wrap">
+        <div className="ticker-move">
+          <div className="ticker-item"><a className="news" href={url}>{title}</a> </div>
+        </div>
+      </div>
+      </div>
+    )
+  }
+}
+
+
