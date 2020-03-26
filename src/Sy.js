c40e6d767a86d2862e49c965db90ab32d139d85a
MansourMoukdad88 <m_sy@outlook.com>
2020-03-26 21:32:54 +0200

    Add Styling

--- src/Sy.js
+++ src/Sy.js
@@ -23,21 +23,22 @@ class Sy extends Component {
     let update = som.global[3]
     return (
       <div className="entireData-items">
-        <h1>Syria</h1>
+        <h1>سوريا</h1>
         <div className="global-item">
           <p style={{"color": "orange", "fontWeight":"bold", "fontSize":"30px"}}> {som.global[0]} </p>
-          <label  style={{"color": "orange", "fontSize":"16px", "fontWeight":"bold",}}>confirmed</label>
+          <label  style={{"color": "orange", "fontSize":"16px", "fontWeight":"bold",}}>المصابون</label>
         </div>
         <div className="global-item">
           <p style={{"color": "green", "fontWeight":"bold", "fontSize":"30px"}}> {som.global[1]}</p>
-          <label style={{"color": "green", "fontSize":"16px", "fontWeight":"bold",}}>recovered</label>
+          <label style={{"color": "green", "fontSize":"16px", "fontWeight":"bold",}}>المتعافون</label>
         </div>
         <div className="global-item">
-          <p style={{"color": "red", "fontWeight":"bold", "fontSize":"30px"}}> {som.global[2]}</p>
-          <label style={{"color": "red", "fontSize":"16px", "fontWeight":"bold",}}>deaths</label>
+          <p style={{"color": "rgba(216, 48, 34", "fontWeight":"bold", "fontSize":"30px"}}> {som.global[2]}</p>
+          <label style={{"color": "rgba(216, 48, 34", "fontSize":"16px", "fontWeight":"bold",}}>الوفيات</label>
         </div>
+
         <br></br><br></br>
-        <div style={{"fontSize":"10px", "color":"white"}}>{ update && (<div>Updated: {new Date(update).toLocaleString()}</div>)}</div>
+        <div style={{"fontSize":"12px", "color":"white"}}>آخر تحديث{ update && (<div> {new Date(update).toLocaleString()}</div>)}</div>
       </div>
     )
   }
